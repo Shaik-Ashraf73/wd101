4be9011d93618b43e7c6906bd9d6e2d0fd72f30b
Shaik-Ashraf73 <151639855+Shaik-Ashraf73@users.noreply.github.com>
2023-11-21 20:43:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,72 @@
-let userForm = document.getElementById("user-form");
-
+// Retrieve entries from localStorage
 const retrieveEntries = () => {
   let entries = localStorage.getItem("user-entries");
-  if (entries) {
-    entries = JSON.parse(entries);
-  } else {
-    entries = [];
-  }
-  return entries;
+  return entries ? JSON.parse(entries) : [];
 };
+
+// Initialize userEntries array
 let userEntries = retrieveEntries();
 
+// Display entries in the HTML table
 const displayEntries = () => {
   const entries = retrieveEntries();
-  const tableEntries = entries
-    .map((entry) => {
-      const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`;
-      const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`;
-      const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`;
-      const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
-      const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndconditions}</td>`;
+  const tableEntries = entries.map((entry) => {
+    const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`;
+    const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`;
+    const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`;
+    const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
+    const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions}</td>`;
 
-      const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
-      return row;
-    })
-    .join("\n");
+    return `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+  }).join("\n");
 
-  const table = `<table class="table-auto w-full"><tr>
+  const table = `<table class="table-auto w-full">
+    <tr>
+      <th class="px-4 py-2">Name</th>
+      <th class="px-4 py-2">Email</th>
+      <th class="px-4 py-2">Password</th>
+      <th class="px-4 py-2">Date of Birth</th>
+      <th class="px-4 py-2">Accepted Terms?</th>
+    </tr>
+    ${tableEntries}
+  </table>`;
 
-    <th class="px-4 py-2">Name</th>
-    <th class="px-4 py-2">Email</th>
-    <th class="px-4 py-2">Password</th>
-    <th class="px-4 py-2">dob</th>
-    <th class="px-4 py-2">accepted terms?</th>
-</tr>${tableEntries} </table>`;
-
-  let details = document.getElementById("user-entries");
-  details.innerHTML = table;
+  // Use consistent variable naming (userEntries vs user-entries)
+  let userEntriesContainer = document.getElementById("user-entries");
+  userEntriesContainer.innerHTML = table;
 };
+
+// Save user form data to localStorage
 const saveUserForm = (event) => {
   event.preventDefault();
+
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const dob = document.getElementById("dob").value;
-  const acceptedTermsAndconditions =
-    document.getElementById("acceptTerms").checked;
+  const acceptedTermsAndConditions = document.getElementById("acceptTerms").checked;
+
   const entry = {
     name,
     email,
     password,
     dob,
-    acceptedTermsAndconditions,
+    acceptedTermsAndConditions,
   };
+
+  // Add entry to userEntries array
   userEntries.push(entry);
 
+  // Save userEntries to localStorage
   localStorage.setItem("user-entries", JSON.stringify(userEntries));
+
+  // Display updated entries in the HTML table
   displayEntries();
 };
+
+// Add event listener to the user form
+let userForm = document.getElementById("user-form");
 userForm.addEventListener("submit", saveUserForm);
+
+// Initial display of entries on page load
 displayEntries();
